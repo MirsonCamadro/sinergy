70aef6c7a75e8395ce65188ea070e48289a6dea1
MirsonCamadro <kztempus@gmail.com>
2019-10-28 16:07:20 -0300

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,59 +18,57 @@ AdminUser.create!(
 Community.create!(
     name: "los volaitos"
 )
-Community.create!(
-    name: "Primero medio del oxicorte"
+User.create!(
+    email: 'bob@marley.com',
+    password: '123123',
+    password_confirmation:'123123'
+    community_id: '1',
+    name: "Bob",
+    last_name: "Marley",
 )
-Community.create!(
-    name: "Viaje a cancun 2021!"
+Act.create!(
+    name: "Acta Nº 1",
+    description: 'Acordamos en comprar, para el siguiente mes, bla bla',
+    date: "2019-09-01",
+    community_id: '1',
+    photo: "https://www.pdffiller.com/preview/420/943/420943346/large.png"
 )
 
+Act.create!(
+    name: "Acta Nº 2",
+    description: 'Acordamos en comprar, para el siguiente mes, bla bla',
+    date: "2019-09-10",
+    community_id: '1',
+    photo: "http://www.aspurc.org.ar/wp-content/uploads/2015/08/ACTA-37.jpg"
+)
 
-5.times do |j|
-    User.create!(
-        email: '',
-        password: '123123',
-        password_confirmation:'123123'
-        community_id: '',
-        name: "volaito#{j+1}",
-        last_name: "delaeskina#{j+1}",
-    )
-end
-
-5.times do |j|
-    User.create!(
-        email: '',
-        password: '123123',
-        password_confirmation:'123123'
-        community_id: '',
-        name: "alumnito#{j+1}",
-        last_name: "delashoreza#{j+1}",
-    )
-end
-
-5.times do |j|
-    User.create!(
-        email: '',
-        password: '123123',
-        password_confirmation:'123123'
-        community_id: '',
-        name: "patiperra#{j+1}",
-        last_name: "socool#{j+1}",
-    )
-end
-
-2.times do |k|
-    Act.create!(
-        name: "Acta Nº#{k+1}",
-        description: 'Acordamos en comprar, para el siguiente mes, bla bla',
-        date: "2019-05-#{k+1}",
-        community_id: '',
-        photo: ""
-    )
-end
-
-
-
-   
-
-
+Fee.create!(
+    user_id: '1',
+    value: '30000',
+    payed: 'false',
+    fees_date: '04-2020'
+)
+Fee.create!(
+    user_id: '1',
+    value: '30000',
+    payed: 'true',
+    fees_date: '04-2020'
+)
+Fee.create!(
+    user_id: '1',
+    value: '30000',
+    payed: 'true',
+    fees_date: '04-2020'
+)   
+Expense.create!(
+    community_id: '1',
+    name: 'Compra de 5 gramos de AK-47',
+    description: 'Se compraron 5g de AK-47 al guaton byron',
+    value: '20000'
+)
+Expense.create!(
+    community_id: '1',
+    name: 'Compra bong para todos',
+    description: 'Se compro un bong de 8 filtros y 3 kilos de hielo',
+    value: '15000'
+)
