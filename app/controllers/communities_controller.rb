0e19387f6c276f1700f178b37973700a5b596cc5
MirsonCamadro <kztempus@gmail.com>
2019-10-29 11:31:18 -0300

    added difference between total payed fees - expeses

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -5,9 +5,16 @@ class CommunitiesController < ApplicationController
       @user = current_user
       @users = User.all
       @community = @user.community_id
+      
       @unpayed_fees = User.unpayed_fees_by_community(current_user.community_id)
       @payed_fees = User.payed_fees_by_community(current_user.community_id)
-  end
+      @total_payed_fees = @payed_fees.inject  {|sum, fee| sum + fee}
+      @total_unpayed_fees = @unpayed_fees.inject  {|sum, fee| sum + fee}
+
+      @expenses_values = @user.community.expenses.pluck('value')
+      @total_expenses = @expenses_values.sum
+      @available = @total_payed_fees - @total_expenses
+    end
   
   
 end
