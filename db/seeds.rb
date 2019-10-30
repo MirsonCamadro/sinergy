d6f074e2ee811f78de0db40ddd33ff8306906f1e
MirsonCamadro <kztempus@gmail.com>
2019-10-30 10:26:40 -0300

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,57 +21,57 @@ AdminUser.create!(
 Community.create!(
     name: "los volaitos"
 )
-User.create!(
-    email: 'bob@marley.com',
-    password: '123123',
-    password_confirmation:'123123'
-    community_id: '1',
-    name: "Bob",
-    last_name: "Marley",
-)
-Act.create!(
-    name: "Acta Nº 1",
-    description: 'Acordamos en comprar, para el siguiente mes, bla bla',
-    date: "2019-09-01",
-    community_id: '1',
-    photo: "https://www.pdffiller.com/preview/420/943/420943346/large.png"
-)
+# User.create!(
+#     email: 'bob@marley.com',
+#     password: '123123',
+#     password_confirmation:'123123'
+#     community_id: '1',
+#     name: "Bob",
+#     last_name: "Marley",
+# )
+# Act.create!(
+#     name: "Acta Nº 1",
+#     description: 'Acordamos en comprar, para el siguiente mes, bla bla',
+#     date: "2019-09-01",
+#     community_id: '1',
+#     photo: "https://www.pdffiller.com/preview/420/943/420943346/large.png"
+# )
 
-Act.create!(
-    name: "Acta Nº 2",
-    description: 'Acordamos en comprar, para el siguiente mes, bla bla',
-    date: "2019-09-10",
-    community_id: '1',
-    photo: "http://www.aspurc.org.ar/wp-content/uploads/2015/08/ACTA-37.jpg"
-)
+# Act.create!(
+#     name: "Acta Nº 2",
+#     description: 'Acordamos en comprar, para el siguiente mes, bla bla',
+#     date: "2019-09-10",
+#     community_id: '1',
+#     photo: "http://www.aspurc.org.ar/wp-content/uploads/2015/08/ACTA-37.jpg"
+# )
 
-Fee.create!(
-    user_id: '1',
-    value: '30000',
-    payed: 'false',
-    fees_date: '04-2020'
-)
-Fee.create!(
-    user_id: '1',
-    value: '30000',
-    payed: 'true',
-    fees_date: '04-2020'
-)
-Fee.create!(
-    user_id: '1',
-    value: '30000',
-    payed: 'true',
-    fees_date: '04-2020'
-)   
-Expense.create!(
-    community_id: '1',
-    name: 'Compra de 5 gramos de AK-47',
-    description: 'Se compraron 5g de AK-47 al guaton byron',
-    value: '20000'
-)
-Expense.create!(
-    community_id: '1',
-    name: 'Compra bong para todos',
-    description: 'Se compro un bong de 8 filtros y 3 kilos de hielo',
-    value: '15000'
-)
+# Fee.create!(
+#     user_id: '1',
+#     value: '30000',
+#     payed: 'false',
+#     fees_date: '04-2020'
+# )
+# Fee.create!(
+#     user_id: '1',
+#     value: '30000',
+#     payed: 'true',
+#     fees_date: '04-2020'
+# )
+# Fee.create!(
+#     user_id: '1',
+#     value: '30000',
+#     payed: 'true',
+#     fees_date: '04-2020'
+# )   
+# Expense.create!(
+#     community_id: '1',
+#     name: 'Compra de 5 gramos de AK-47',
+#     description: 'Se compraron 5g de AK-47 al guaton byron',
+#     value: '20000'
+# )
+# Expense.create!(
+#     community_id: '1',
+#     name: 'Compra bong para todos',
+#     description: 'Se compro un bong de 8 filtros y 3 kilos de hielo',
+#     value: '15000'
+# )
